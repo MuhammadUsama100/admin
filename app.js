c2e49e624bb4ac947588ed4fca3a2dfb793b3eca
Muhammad Usama <usamamuhammad100@gmail.com>
2021-01-15 22:18:01 +0500

    initial

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
 const mongoose = require("mongoose");
 const db =
   "mongodb+srv://usama:usama@cluster0.rzfij.mongodb.net/Cluster0?retryWrites=true&w=majority";
@@ -30,10 +28,9 @@ connectDB();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
@@ -43,7 +40,7 @@ app.use("/users", usersRouter);
 app.use(function (req, res, next) {
   next(createError(404));
 });
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
